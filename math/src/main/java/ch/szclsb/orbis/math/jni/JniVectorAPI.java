a3919defe20fac4d009a629a34a5abf206bcf2dd
Claudio <szclsb@gmail.com>
2021-09-26 23:26:42 +0200

    prepared jni vector

--- math/src/main/java/ch/szclsb/orbis/math/jni/JniVectorAPI.java
+++ math/src/main/java/ch/szclsb/orbis/math/jni/JniVectorAPI.java
@@ -1,6 +1,10 @@
 package ch.szclsb.orbis.math.jni;
 
-public class JniVectorAPI {
+public final class JniVectorAPI {
+    static {
+        System.load(System.getProperty("user.dir") + "/libs/orbis_native.dll");
+    }
+
     public static native void cAdd(int size, float[] r, float[] a, float[] b);
     public static native void cSub(int size, float[] r, float[] a, float[] b);
     public static native void cMul(int size, float[] r, float[] a, float[] b);
